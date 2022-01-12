56ae7bd9142b4ad2e06fa617d716261b476e9166
Helio Brandao <h.brandao@outlook.com.br>
2022-01-12 20:38:25 -0300

    Falha Netlify Movie Card

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -12,11 +12,13 @@ function MovieCard() {
         score: 4.5
     };
 
+
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
+                
                 <MovieScore />
 
                 <Link to={'/form/${movie.id}'}>
